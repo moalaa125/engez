6582d2f4127109784ce8c35b6c208d3ccde7a0ad
moalaa125 <muhamedawadkassem@gmail.com>
2026-07-09 02:59:37 +0300

    make the otp screen scrollable

--- lib/features/auth/presentation/screens/otp_screen.dart
+++ lib/features/auth/presentation/screens/otp_screen.dart
@@ -90,31 +90,33 @@ class _MyWidgetState extends State<OtpScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Hero(
-              tag: 'animationForEngezTxt',
-              child: Material(
-                type: MaterialType.transparency,
-                child: Text(
-                  'إنجز',
-                  style: TextStyle(
-                    color: MyColors.myLighterGreen,
-                    fontSize: 40.sp,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'aref',
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Hero(
+                tag: 'animationForEngezTxt',
+                child: Material(
+                  type: MaterialType.transparency,
+                  child: Text(
+                    'إنجز',
+                    style: TextStyle(
+                      color: MyColors.myLighterGreen,
+                      fontSize: 40.sp,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'aref',
+                    ),
                   ),
                 ),
               ),
-            ),
-            _introImage(),
-            _buildOtpCodeSection(),
-            SizedBox(height: 30.h),
-            _buildButton(),
-            SizedBox(height: 10.h),
-            _buildEndTexts(),
-          ],
+              _introImage(),
+              _buildOtpCodeSection(),
+              SizedBox(height: 30.h),
+              _buildButton(),
+              SizedBox(height: 10.h),
+              _buildEndTexts(),
+            ],
+          ),
         ),
       ),
     );
