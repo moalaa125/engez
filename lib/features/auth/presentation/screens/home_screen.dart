6b74e8b7c15bdce1d674f7425c162154ed331c1f
moalaa125 <muhamedawadkassem@gmail.com>
2026-08-01 18:37:08 +0300

    refactrot the nav bar code

--- lib/features/auth/presentation/screens/home_screen.dart
+++ lib/features/auth/presentation/screens/home_screen.dart
@@ -3,11 +3,11 @@ import 'package:engez/features/location/manger/location_cubit.dart';
 import 'package:engez/features/location/manger/location_state.dart';
 import 'package:engez/widgets/custom_icon_button.dart';
 import 'package:engez/widgets/custom_offer_section.dart';
+import 'package:engez/widgets/nav_bar.dart';
 import 'package:engez/widgets/place_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   // final String verificationId;
@@ -19,7 +19,6 @@ class HomeScreen extends StatefulWidget {
 
 class _MyWidgetState extends State<HomeScreen> {
   late final LocationCubit _locationCubit;
-  int _currentIndex = 0;
 
   @override
   void initState() {
@@ -261,88 +260,7 @@ class _MyWidgetState extends State<HomeScreen> {
   }
 
   Widget _buildBottomNavBar() {
-    return Container(
-      decoration: BoxDecoration(
-        color: MyColors.myWhite,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withValues(alpha: 0.06),
-            blurRadius: 12,
-            offset: const Offset(0, -2),
-          ),
-        ],
-      ),
-      child: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 4.h),
-          child: Padding(
-            padding: const EdgeInsets.only(top: 5),
-            child: GNav(
-              rippleColor: MyColors.myOrange.withValues(alpha: 0.1),
-              hoverColor: MyColors.myOrange.withValues(alpha: 0.05),
-              gap: 6.w,
-              activeColor: MyColors.myOrange,
-              iconSize: 25.sp,
-              duration: const Duration(milliseconds: 350),
-              curve: Curves.easeOutExpo,
-              color: Colors.grey[500],
-              tabBackgroundColor: MyColors.myOrange.withValues(alpha: 0.1),
-              backgroundColor: MyColors.myWhite,
-              padding: EdgeInsets.symmetric(horizontal: 14.w, vertical: 6.h),
-              tabBorderRadius: 16,
-              tabs: [
-                GButton(
-                  icon: Icons.home_outlined,
-                  text: 'الرئيسية',
-                  textStyle: TextStyle(
-                    fontFamily: 'cairo',
-                    fontSize: 13.sp,
-                    color: MyColors.myOrange,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                GButton(
-                  icon: Icons.search,
-                  text: 'بحث',
-                  textStyle: TextStyle(
-                    fontFamily: 'cairo',
-                    fontSize: 13.sp,
-                    color: MyColors.myOrange,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                GButton(
-                  icon: Icons.help_outline,
-                  text: 'طلباتي',
-                  textStyle: TextStyle(
-                    fontFamily: 'cairo',
-                    fontSize: 13.sp,
-                    color: MyColors.myOrange,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                GButton(
-                  icon: Icons.person_outline,
-                  text: 'حسابي',
-                  textStyle: TextStyle(
-                    fontFamily: 'cairo',
-                    fontSize: 13.sp,
-                    color: MyColors.myOrange,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ],
-              selectedIndex: _currentIndex,
-              onTabChange: (index) {
-                setState(() {
-                  _currentIndex = index;
-                });
-              },
-            ),
-          ),
-        ),
-      ),
-    );
+    return  NavBar();
   }
 
   @override
---
+++ lib/widgets/nav_bar.dart
@@ -0,0 +1,101 @@
+import 'package:engez/constants/my_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class NavBar extends StatefulWidget {
+  const NavBar({super.key});
+
+  @override
+  State<NavBar> createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: MyColors.myWhite,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.06),
+            blurRadius: 12,
+            offset: const Offset(0, -2),
+          ),
+        ],
+      ),
+      child: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 4.h),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 5),
+            child: GNav(
+              rippleColor: MyColors.myOrange.withValues(alpha: 0.1),
+              hoverColor: MyColors.myOrange.withValues(alpha: 0.05),
+              gap: 6.w,
+              activeColor: MyColors.myOrange,
+              iconSize: 25.sp,
+              duration: const Duration(milliseconds: 350),
+              curve: Curves.easeOutExpo,
+              color: Colors.grey[500],
+              tabBackgroundColor: MyColors.myOrange.withValues(alpha: 0.1),
+              backgroundColor: MyColors.myWhite,
+              padding: EdgeInsets.symmetric(horizontal: 14.w, vertical: 6.h),
+              tabBorderRadius: 16,
+              tabs: [
+                GButton(
+                  icon: Icons.home_outlined,
+                  text: 'الرئيسية',
+                  textStyle: TextStyle(
+                    fontFamily: 'cairo',
+                    fontSize: 13.sp,
+                    color: MyColors.myOrange,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                GButton(
+                  icon: Icons.search,
+                  text: 'بحث',
+                  textStyle: TextStyle(
+                    fontFamily: 'cairo',
+                    fontSize: 13.sp,
+                    color: MyColors.myOrange,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                GButton(
+                  icon: Icons.help_outline,
+                  text: 'طلباتي',
+                  textStyle: TextStyle(
+                    fontFamily: 'cairo',
+                    fontSize: 13.sp,
+                    color: MyColors.myOrange,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                GButton(
+                  icon: Icons.person_outline,
+                  text: 'حسابي',
+                  textStyle: TextStyle(
+                    fontFamily: 'cairo',
+                    fontSize: 13.sp,
+                    color: MyColors.myOrange,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+              selectedIndex: _currentIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
