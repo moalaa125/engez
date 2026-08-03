e2432482a070353f391f5899ff6e36705442a54a
moalaa125 <muhamedawadkassem@gmail.com>
2026-08-03 17:32:40 +0300

    update kbab screen

--- lib/features/auth/presentation/screens/kbab.dart
+++ lib/features/auth/presentation/screens/kbab.dart
@@ -32,9 +32,7 @@ class Kbab extends StatelessWidget {
                   },
             style: ElevatedButton.styleFrom(
               backgroundColor: MyColors.myOrange,
-              disabledBackgroundColor: MyColors.myOrange.withValues(
-                alpha: 0.4,
-              ),
+              disabledBackgroundColor: MyColors.myOrange.withValues(alpha: 0.4),
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(20.r),
               ),
@@ -157,6 +155,139 @@ class Kbab extends StatelessWidget {
     return const CategoryList();
   }
 
+  Widget _buildImageAndDetailsBar() {
+    return SizedBox(
+      height: 400.h,
+      child: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            height: 330.h,
+            child: Hero(
+              tag: 'kbab_basha_tag',
+              child: Image.asset(
+                'assets/images/kbabbasha.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 16.w,
+            right: 16.w,
+            child: Container(
+              height: 140.h,
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+              decoration: BoxDecoration(
+                color: MyColors.myWhite,
+                borderRadius: BorderRadius.circular(20.r),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withValues(alpha: .1),
+                    blurRadius: 10,
+                    spreadRadius: 2,
+                    offset: const Offset(0, 5),
+                  ),
+                ],
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Kbab Basha',
+                          style: TextStyle(
+                            fontSize: 24.sp,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 4.h),
+                        Text(
+                          'Grill & smash burger',
+                          style: TextStyle(
+                            fontSize: 16.sp,
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                        SizedBox(height: 12.h),
+                        Divider(
+                          color: Colors.grey.withValues(alpha: .3),
+                          height: 2,
+                          thickness: 1,
+                        ),
+                        SizedBox(height: 12.h),
+                        Row(
+                          children: [
+                            const Icon(
+                              Icons.directions_walk,
+                              size: 18,
+                              color: Colors.brown,
+                            ),
+                            SizedBox(width: 6.w),
+                            Text(
+                              '30 min',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14.sp,
+                              ),
+                            ),
+                            SizedBox(width: 10.w),
+                            const CircleAvatar(
+                              radius: 3,
+                              backgroundColor: Colors.grey,
+                            ),
+                            SizedBox(width: 10.w),
+                            Text(
+                              'Delivery available',
+                              style: TextStyle(
+                                color: Colors.grey[700],
+                                fontSize: 14.sp,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 10.w,
+                      vertical: 6.h,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.r),
+                      border: Border.all(color: Colors.grey.shade300),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Icon(Icons.star, color: Colors.orange, size: 16),
+                        SizedBox(width: 4.w),
+                        Text(
+                          '4.9',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 13.sp,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -168,143 +299,7 @@ class Kbab extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(
-              height: 400.h,
-              child: Stack(
-                children: [
-                  Positioned(
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    height: 330.h,
-                    child: Hero(
-                      tag: 'kbab_basha_tag',
-                      child: Image.asset(
-                        'assets/images/kbabbasha.png',
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    left: 16.w,
-                    right: 16.w,
-                    child: Container(
-                      height: 140.h,
-                      width: double.infinity,
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 16.w,
-                        vertical: 16.h,
-                      ),
-                      decoration: BoxDecoration(
-                        color: MyColors.myWhite,
-                        borderRadius: BorderRadius.circular(20.r),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withValues(alpha: .1),
-                            blurRadius: 10,
-                            spreadRadius: 2,
-                            offset: const Offset(0, 5),
-                          ),
-                        ],
-                      ),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Kbab Basha',
-                                  style: TextStyle(
-                                    fontSize: 24.sp,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                SizedBox(height: 4.h),
-                                Text(
-                                  'Grill & smash burger',
-                                  style: TextStyle(
-                                    fontSize: 16.sp,
-                                    color: Colors.grey[700],
-                                  ),
-                                ),
-                                SizedBox(height: 12.h),
-                                Divider(
-                                  color: Colors.grey.withValues(alpha: .3),
-                                  height: 2,
-                                  thickness: 1,
-                                ),
-                                SizedBox(height: 12.h),
-                                Row(
-                                  children: [
-                                    const Icon(
-                                      Icons.directions_walk,
-                                      size: 18,
-                                      color: Colors.brown,
-                                    ),
-                                    SizedBox(width: 6.w),
-                                    Text(
-                                      '30 min',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 14.sp,
-                                      ),
-                                    ),
-                                    SizedBox(width: 10.w),
-                                    const CircleAvatar(
-                                      radius: 3,
-                                      backgroundColor: Colors.grey,
-                                    ),
-                                    SizedBox(width: 10.w),
-                                    Text(
-                                      'Delivery available',
-                                      style: TextStyle(
-                                        color: Colors.grey[700],
-                                        fontSize: 14.sp,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                          Container(
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 10.w,
-                              vertical: 6.h,
-                            ),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20.r),
-                              border: Border.all(color: Colors.grey.shade300),
-                            ),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                const Icon(
-                                  Icons.star,
-                                  color: Colors.orange,
-                                  size: 16,
-                                ),
-                                SizedBox(width: 4.w),
-                                Text(
-                                  '4.9',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 13.sp,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            _buildImageAndDetailsBar(),
             SizedBox(height: 20.h),
             _buildListOfTextButtons(),
             SizedBox(height: 20.h),
